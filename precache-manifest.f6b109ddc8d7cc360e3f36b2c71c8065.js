ed46bb9a355ad44ccb0510f51e0c378355a7193a
Jkev <endlezzjkev@gmail.com>
2020-10-28 18:47:52 -0600

    Updates

---
+++ precache-manifest.f6b109ddc8d7cc360e3f36b2c71c8065.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd981358284c12256f9ec77e5f81d75a",
+    "url": "/Jkev/index.html"
+  },
+  {
+    "revision": "c08520b2c43c921e0e17",
+    "url": "/Jkev/static/css/main.dc0cc47c.chunk.css"
+  },
+  {
+    "revision": "7a5d0420344da5e5803c",
+    "url": "/Jkev/static/js/2.addae683.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/Jkev/static/js/2.addae683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08520b2c43c921e0e17",
+    "url": "/Jkev/static/js/main.7085d1fb.chunk.js"
+  },
+  {
+    "revision": "3723626ebe5331249cd6",
+    "url": "/Jkev/static/js/runtime-main.520ca182.js"
+  },
+  {
+    "revision": "6a403a0894517cbda7e87816eb23dd54",
+    "url": "/Jkev/static/media/airbnb.6a403a08.png"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "/Jkev/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "/Jkev/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "/Jkev/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "c07556c32764f6d706fc7e4a15299149",
+    "url": "/Jkev/static/media/covidtracker.c07556c3.jpg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/Jkev/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/Jkev/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "/Jkev/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "0555bfb7e81f57a1efe6a53c7490abb7",
+    "url": "/Jkev/static/media/github-api.0555bfb7.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/Jkev/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/Jkev/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "/Jkev/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "/Jkev/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "cb7918700224cbdc2c62c2368a45d4d2",
+    "url": "/Jkev/static/media/jkev.cb791870.jpg"
+  },
+  {
+    "revision": "d8f5bb6956e35e636692dd556353a1fe",
+    "url": "/Jkev/static/media/jkev.d8f5bb69.jpg"
+  },
+  {
+    "revision": "aaf040aa0d76994dc04917f3dd46e214",
+    "url": "/Jkev/static/media/jkev1.aaf040aa.jpg"
+  },
+  {
+    "revision": "195a29a19f5fdc89c428136cc15675ad",
+    "url": "/Jkev/static/media/jkev2.195a29a1.jpg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "/Jkev/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "9e6687b676cf7cb81ae6ae68e9741052",
+    "url": "/Jkev/static/media/netflixclone.9e6687b6.png"
+  },
+  {
+    "revision": "10a45acb6bd2ba984fc9c046c4a12afe",
+    "url": "/Jkev/static/media/newslatter.10a45acb.png"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/Jkev/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "ef194ddf5faf63cb6cc580a4535be695",
+    "url": "/Jkev/static/media/react-bootstrap.ef194ddf.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/Jkev/static/media/react-router.02419671.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/Jkev/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/Jkev/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "dab06e75730ade885504c1800d6306e0",
+    "url": "/Jkev/static/media/setupjkev.dab06e75.jpeg"
+  }
+]);
